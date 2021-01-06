51bde63913599701ce294bdf379a8f969266ca90
snek <40864710+snekplex@users.noreply.github.com>
2021-01-06 15:27:17 -0600

    Added /api/endpoints router

--- server/routing/endpoints.js
+++ server/routing/endpoints.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-router.get('/api/health-check', (req, res) => {
+router.get('/health-check', (req, res) => {
     res.json({
         working: true
-    });
+    })
 })
-
-router.use('/api/endpoints', require('./endpoints'))
 
 module.exports = router;
