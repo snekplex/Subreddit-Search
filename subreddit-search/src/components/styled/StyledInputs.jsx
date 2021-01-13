b791903288f1a844824e9f0ac72051e49c0ca0ba
snek <40864710+snekplex@users.noreply.github.com>
2021-01-13 13:02:40 -0600

    Created basic styled SearchInput

---
+++ subreddit-search/src/components/styled/StyledInputs.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledSearchInput = styled.input`
+    font-size: 1.5em;
+    font-family: 'Rubik', sans-seriff;
+    border: 2px solid;
+    border-radius: 5px;
+    padding-left: 0.5em;
+
+`
