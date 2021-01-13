6eb39fd1dbb5ccf856bd81da48c2277b7a2d9622
snek <40864710+snekplex@users.noreply.github.com>
2021-01-13 13:02:16 -0600

    Added button theme properties

--- subreddit-search/src/components/styled/StyledThemes.jsx
+++ subreddit-search/src/components/styled/StyledThemes.jsx
@@ -1,3 +1,15 @@
 export const defaultTheme = {
+    wrappers: {
 
+    },
+    buttons: {
+        background: 'white',
+        color: 'black',
+        border: '2px solid black',
+        borderRadius: '10px',
+        fontFamily: "'Rubik', sans-seriff"
+    },
+    inputs: {
+        
+    }
 }
