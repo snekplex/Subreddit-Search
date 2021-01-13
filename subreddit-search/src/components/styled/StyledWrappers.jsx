de9f42b85f8a3505fbf5ab418ac08eb6c72e58be
snek <40864710+snekplex@users.noreply.github.com>
2021-01-13 13:00:34 -0600

    Added SearchBoxWrapper

--- subreddit-search/src/components/styled/StyledWrappers.jsx
+++ subreddit-search/src/components/styled/StyledWrappers.jsx
@@ -5,3 +5,10 @@ export const StyledBasicWrapper = styled.div`
     align-items: center;
     justify-content: center;
 `;
+
+export const StyledSearchBoxWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
