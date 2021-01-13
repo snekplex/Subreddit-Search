1b4e67e67b1117315934c30d261f0d25c024b6ff
snek <40864710+snekplex@users.noreply.github.com>
2021-01-13 13:01:15 -0600

    Updated the styled wrapper to SearchBox

--- subreddit-search/src/components/custom/AppWraper.jsx
+++ subreddit-search/src/components/custom/AppWraper.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyledBasicWrapper } from '../styled/StyledWrappers';
+import { StyledBasicWrapper, StyledSearchBoxWrapper } from '../styled/StyledWrappers';
+import { StyledSubmitBtn } from '../styled/StyledButtons';
+import { StyledSearchInput } from '../styled/StyledInputs';
 
 export default class AppWrapper extends React.Component {
     render() {
         return (
             <div>
-                <StyledBasicWrapper>
-                </StyledBasicWrapper>
+                <StyledSearchBoxWrapper>
+                    <StyledSearchInput placeholder="Enter search term here..."/>
+                    <StyledSubmitBtn>
+                        Submit
+                    </StyledSubmitBtn>
+                </StyledSearchBoxWrapper>
             </div>
         )
     }
