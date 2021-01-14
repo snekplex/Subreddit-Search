6883bb14ba27c5c224605581ee6f4b67fa88a589
snek <40864710+snekplex@users.noreply.github.com>
2021-01-14 11:59:47 -0600

    Added better code convetions for theme

--- subreddit-search/src/components/styled/StyledButtons.jsx
+++ subreddit-search/src/components/styled/StyledButtons.jsx
@@ -3,12 +3,12 @@ import styled from 'styled-components';
 export const StyledSubmitBtn = styled.button`
     font-size: 1em;
     padding: 1em 2em 1em 2em;
-    font-family: ${props => props.theme.buttons.fontFamily};
+    font-family: ${props => props.theme.buttons.submit.fontFamily};
     cursor: pointer;
     font-weight: 700;
     text-align: center;
-    background: ${props => props.theme.buttons.background};
-    color: ${props => props.theme.buttons.color};
-    border: ${props =>  props.theme.buttons.border};
-    border-radius: ${props => props.theme.buttons.borderRadius}
+    background: ${props => props.theme.buttons.submit.background};
+    color: ${props => props.theme.buttons.submit.color};
+    border: ${props =>  props.theme.buttons.submit.border};
+    border-radius: ${props => props.theme.buttons.submit.borderRadius}
 `;
--- subreddit-search/src/components/styled/StyledThemes.jsx
+++ subreddit-search/src/components/styled/StyledThemes.jsx
@@ -3,11 +3,13 @@ export const defaultTheme = {
 
     },
     buttons: {
-        background: 'white',
-        color: 'black',
-        border: '2px solid black',
-        borderRadius: '10px',
-        fontFamily: "'Rubik', sans-seriff"
+        submit: {
+            background: 'white',
+            color: 'black',
+            border: '2px solid black',
+            borderRadius: '10px',
+            fontFamily: "'Rubik', sans-seriff"
+        }
     },
     inputs: {
         
