98dcd9cd16f71d97ec65e4f2293901baccacb0bc
snek <40864710+snekplex@users.noreply.github.com>
2021-01-19 12:14:32 -0600

    Basic app that allows searching of api

--- subreddit-search/src/components/custom/AppWraper.jsx
+++ subreddit-search/src/components/custom/AppWraper.jsx
@@ -1,17 +1,93 @@
 import React from 'react';
+import BeatLoader from 'react-spinners/BeatLoader';
 import { StyledBasicWrapper, StyledSearchBoxWrapper } from '../styled/StyledWrappers';
 import { StyledSubmitBtn } from '../styled/StyledButtons';
-import { StyledSearchInput } from '../styled/StyledInputs';
+import { StyledSearchInput, StyledTextArea } from '../styled/StyledInputs';
+
+import * as searchService from '../../services/api.searchService';
 
 export default class AppWrapper extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loadingResults: false,
+            searchString: '',
+            subredditsString: '',
+            searchTerms: [],
+            subreddits: [],
+            results: {}
+        }
+    }
+
+    onSearchInputChange = (input) => {
+        this.setState({
+            searchString: input
+        })
+    }
+
+    onSubrreditSearchChange = (input) => {
+        this.setState({
+            subredditsString: input
+        })
+    }
+
+    onSearchSubmit = async () => {
+        this.setState({ loadingResults: true })
+
+        const searchTerms = this.state.searchString.trim()
+                            .split(' ');
+
+        const subreddits = this.state.subredditsString.trim()
+                           .split('\n');
+
+        // Removes any potential white-space before/after subreddit
+        for (let i=0; i < subreddits.length; i++) {
+            subreddits[i] = subreddits[i].trim()
+        }
+
+        const results = await searchService.getSearchResults(
+            searchTerms,
+            subreddits
+        ).catch((err) => {
+            console.log(err)
+        })
+
+        this.setState({
+            searchTerms: searchTerms,
+            subreddits: subreddits,
+            results: results,
+            loadingResults: false
+        })
+    }
+
+
     render() {
         return (
             <div>
                 <StyledSearchBoxWrapper>
-                    <StyledSearchInput placeholder="Enter search term here..."/>
-                    <StyledSubmitBtn>
+                    <StyledSearchInput 
+                        placeholder="Enter search term here..."
+                        onChange={(event) => this.onSearchInputChange(event.target.value) }
+                    />
+                    <StyledTextArea
+                        placeholder="Enter subreddits here seperated by newline"
+                        onChange={(event) => this.onSubrreditSearchChange(event.target.value) } 
+                    />
+                    <StyledSubmitBtn onClick={() => this.onSearchSubmit()}>
                         Submit
                     </StyledSubmitBtn>
+                    <div>
+                        {
+                            this.state.loadingResults && (
+                                <BeatLoader
+                                    size={40}
+                                    color={"#000"}
+                                    loading={this.state.loadingResults}
+                                />
+                            )
+                        }
+                    </div>
                 </StyledSearchBoxWrapper>
             </div>
         )
