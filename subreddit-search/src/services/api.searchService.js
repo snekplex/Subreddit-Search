d2af6c22ee6815ce8d0cfca96cdf1e262a11e7c5
snek <40864710+snekplex@users.noreply.github.com>
2021-01-19 12:12:18 -0600

    Added service to get search results from backend

---
+++ subreddit-search/src/services/api.searchService.js
@@ -0,0 +1,28 @@
+
+export async function getSearchResults(searchTerms, subreddits) {
+    // searchTerms: array of terms to search
+    // subreddits: an arrary of subreddits to query for the search terms
+
+    var url = 'http://localhost:3000/api/endpoints/search?'
+
+    for (let i=0; i < searchTerms.length; i++) {
+        if (i === 0) {
+            url += `searchTerms=${searchTerms[i]}`
+        } else {
+            url += `&searchTerms=${searchTerms[i]}`
+        }
+    }
+    
+    for (let i=0; i < subreddits.length; i++) {
+        url += `&subreddits=${subreddits[i]}`
+    }
+
+    const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors',
+    }).catch((err) => {
+        console.log(err);
+    })
+
+    return response.json()
+}
