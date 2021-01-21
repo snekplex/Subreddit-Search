523d3d9eca8d492620759df3d8757a57a53ed2f4
snek <40864710+snekplex@users.noreply.github.com>
2021-01-21 12:06:30 -0600

    Added StyledWebsiteTitle/Subtitle

---
+++ subreddit-search/src/components/styled/StyledTitles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledWebsiteTitle = styled.h1`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: 'Rubik', sans-seriff;
+`;
+
+export const StyledWebsiteSubtitle = styled.h3`
+    font-family: 'Rubik', sans-seriff;
+`;
