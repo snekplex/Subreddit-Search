ba3c3178f382e89da96f2b01fdd99909f81c7bae
snek <40864710+snekplex@users.noreply.github.com>
2021-01-21 12:07:31 -0600

    Added StyledWebsiteTitleWrapper/ContentWrapper

--- subreddit-search/src/components/styled/StyledWrappers.jsx
+++ subreddit-search/src/components/styled/StyledWrappers.jsx
@@ -4,6 +4,25 @@ export const StyledBasicWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    width: 100%;
+    height: 100%;
+`;
+
+export const StyledWebsiteContentWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 100%;
+    height: 100%
+`;
+
+export const StyledWebsiteTitleWrapper = styled.div`
+    disply: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    align-text: center;
 `;
 
 export const StyledSearchBoxWrapper = styled.div`
