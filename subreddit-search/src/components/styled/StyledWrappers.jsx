ef6d9ac1af5cf1bddbd24a0d5ef1c4e9270fb2e5
snek <40864710+snekplex@users.noreply.github.com>
2021-01-27 15:13:59 -0600

    Added direction to column for Basic

--- subreddit-search/src/components/styled/StyledWrappers.jsx
+++ subreddit-search/src/components/styled/StyledWrappers.jsx
@@ -4,6 +4,7 @@ export const StyledBasicWrapper = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
     width: 100%;
     height: 100%;
 `;
