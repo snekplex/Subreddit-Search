1294c8fe6be227c244a30cb60ada8fd54ad3e2f1
snek <40864710+snekplex@users.noreply.github.com>
2021-02-04 12:34:01 -0600

    Styled and created workable result components

--- subreddit-search/src/components/custom/SearchResults.jsx
+++ subreddit-search/src/components/custom/SearchResults.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import { StyledResultSectionTitle } from '../styled/StyledTitles';
-import { StyledResultSection } from '../styled/StyledSections';
-import { StyledResultsWrapper } from '../styled/StyledWrappers';
+import { StyledResultSectionTitle,
+         StyledPostTitle } from '../styled/StyledTitles';
+import { StyledResultSection,
+         StyledResultsSections } from '../styled/StyledSections';
+import { StyledResultsWrapper,
+         StyledResultWrapper } from '../styled/StyledWrappers';
+import { StyledResultImage,
+         StyledResult,
+         StyledResultBody,
+         StyledResultFooter, 
+         StyledResultHeader,
+         StyledResultSubredditLink } from '../styled/StyledContent';
+import ReactPlayer from 'react-player'
 
 export class ResultsSections extends React.Component {
     constructor(props) {
@@ -13,7 +23,7 @@ export class ResultsSections extends React.Component {
 
     render() {
         return (
-            <div>
+            <StyledResultsSections>
                 {
                     this.props.apiData.results &&
                     Object.keys(this.props.apiData.results).map((subreddit) => (
@@ -24,7 +34,7 @@ export class ResultsSections extends React.Component {
                         />
                     ))
                 }
-            </div>
+            </StyledResultsSections>
         )
     }
 }
@@ -100,32 +110,140 @@ class Result extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
         }
     }
 
-    evaulatePostOutboundLink() {
-        // See what each link is. Is it to a website, img, etc.
+    evaulatePostOutboundLink(link) {
+        if (link === undefined || link === null) {
+            return link;
+        } 
+
+        var linkType;
+        
+        const extensions = {
+            img: [
+                'png',
+                'jpg',
+                'gif',
+            ],
+            video: [
+                'mp4',
+                'webm',
+            ],
+            conversion: [
+                'gifv'
+            ]
+        }
+
+        var fileExt = link.split('.').pop()
+
+        if (extensions.img.includes(fileExt)) {
+            linkType = 'image'
+        } else if (
+            extensions.video.includes(fileExt) ||
+            link.includes('youtube')
+        ) {
+            linkType = 'video'
+        } else if (extensions.conversion.includes(fileExt)) {
+            linkType = 'conversion'
+        } else {
+            linkType = 'unknown'
+        }
+
+        return linkType
+    }
+
+    convertTo(link) {
+        const conversions = {
+            'gifv': 'mp4'
+        }
+
+        var fileExt = link.split('.').pop()
+
+        if (conversions[fileExt]) {
+            return conversions[fileExt]
+        } else {
+            return null;
+        }
+    }
+
+    convertLink(link) {
+        const conversions = {
+            'gifv': 'mp4'
+        }
+
+        var fileExt = link.split('.').pop()
+
+        if (conversions[fileExt]) {
+            // Should work most of the time in this use case.
+            // Though if a file has a file type in its name, it will 
+            // most likely fail.
+            var newLink = link.replace(fileExt, conversions[fileExt])
+            return newLink
+        } else {
+            return link
+        }
+
+    }
+
+    openPostInNewTab() {
+        const newWindow = window.open(this.props.postLink, '_blank', 'noopener,noreferrer');
+        if (newWindow) newWindow.opener = null;
     }
 
     render () {
         return (
-            <div>
-                <div>
-                    {/* Title card */}
-                    <h5>{ this.props.title }</h5>
-                    <h6>{ this.props.subreddit }</h6>
-                </div>
-                <div>
-                    {/* Content to display if any. (img, embedded link, etc)*/}
-                    { this.props.postLink }
-                    { this.props.postOutboundLink }
-                </div>
-                <div>
-                    {/* Footer containing other metadata like tags */}
-                    { this.props.tags }
-                </div>
-            </div>
+            <StyledResultWrapper>
+                <StyledResult onClick={() => this.openPostInNewTab()}>
+                    <StyledResultHeader>
+                        <StyledPostTitle>
+                            { this.props.title }
+                        </StyledPostTitle>
+                        <div>
+                            <StyledResultSubredditLink href={'https://reddit.com/r/'+this.props.subreddit}>
+                                r/{this.props.subreddit}
+                            </StyledResultSubredditLink>
+                        </div>
+                    </StyledResultHeader>
+                    <StyledResultBody>
+                        {
+                            this.evaulatePostOutboundLink(this.props.postOutboundLink) === 'image' &&
+                            (
+                                <StyledResultImage
+                                    src={this.props.postOutboundLink}
+                                    alt={this.props.postOutboundLink}
+                                />
+                            )
+                        }
+                        {
+                            this.evaulatePostOutboundLink(this.props.postOutboundLink) === 'video' &&
+                            (
+                                <ReactPlayer
+                                    width="auto"
+                                    height="auto"
+                                    url={this.props.postOutboundLink}
+                                    controls={true}
+                                />
+                            )
+                        }
+                        {
+                            this.evaulatePostOutboundLink(this.props.postOutboundLink) === 'conversion' &&
+                            this.convertTo(this.props.postOutboundLink) === 'mp4' && 
+                            (
+                                <ReactPlayer
+                                    width="100%"
+                                    height="100%"
+                                    url={this.convertLink(this.props.postOutboundLink)}
+                                    controls={true}
+                                />
+                            )
+                        }
+                    </StyledResultBody>
+                    <StyledResultFooter>
+                        Tags: { this.props.tags.map((tag) => ( <span key={Math.random()}>| {tag} |</span> )) }
+                    </StyledResultFooter>
+                </StyledResult>
+            </StyledResultWrapper>
         )
     }
 }
